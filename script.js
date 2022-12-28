75ef704f263554caacbd697f7f6a6fc633c32fe5
ADHAMBENNOUR <52788235+ADHAMBENNOUR@users.noreply.github.com>
2022-12-28 14:52:57 +0100

    added new code

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination2",
+      dynamicBullets: true,
+    },
+  });
+
+ 
